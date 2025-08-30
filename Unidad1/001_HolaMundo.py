1d568c6deaefa762b6abaa9f562847951c5539b2
x <leon.ramos@zapopan.tecmm.edu.mx>
2025-08-30 09:04:42 -0600

    version 1: Primeros pasos en Python
    se actualizo la practica 001 de Primeros pasos en Python

---
+++ Unidad1/001_HolaMundo.py
@@ -0,0 +1,28 @@
+print("Hola Python")
+
+x = 5
+
+print(x)
+#salida: 5
+
+
+# Declaramos las variables a, b
+# y asignamos dos valores
+a = 3
+b = 7
+z = 0
+
+print(a+b)
+
+print(z)
+# NameError: name 'z' is not defined
+
+a = 10
+if a == 10:
+    print("Es 10")
+else:
+    print("No es 10")
+
+valor_decimal = 10.3234
+
+mi_cadena = "Hola Mundo"
